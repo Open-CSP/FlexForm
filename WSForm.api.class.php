5687d45300be122d7fbb297bd1c287a4132fbba9
designburo <info@designburo.nl>
2021-06-21 21:51:43 +0200

    v 0.8.0.9.7.5 : changed behaviour for new version of select2

--- WSForm.api.class.php
+++ WSForm.api.class.php
@@ -277,9 +277,9 @@ class wbApi {
       if ( isset( $config['wgScript'] ) && $config['wgScript'] !== '' ) {
           $this->app['wgScript'] = rtrim( $config['wgScript'], '/' );
           // /w/index.php -> /w
-          $withoutIndex = str_replace('/index.php/', '',  $this->app['wgScript'] );
+          $withoutIndex = str_replace('/index.php', '',  $this->app['wgScript'] );
           if( !empty( $withoutIndex ) ) {
-              $this->app['IP'] =  $this->app['IP'] . '/' . $withoutIndex;
+              $this->app['IP'] =  $this->app['IP'] .  ltrim($withoutIndex, '/' );
           }
       } else {
           $this->app['wgScript'] = "/index.php";
