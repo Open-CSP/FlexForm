a643bedfbc7a2f49e85462e392b0e3d19af49c63
designburo <info@designburo.nl>
2021-08-19 14:24:51 +0200

    multiple instance and showonselect 4

--- classes/instance.class.php
+++ classes/instance.class.php
@@ -12,8 +12,82 @@ namespace wsform\instance;
 
 use wsform\validate\validate;
 use wsform\wsform;
+use MediaWiki\Revision\RevisionRecord;
 
 class render {
+
+
+	private static function instanceDefault( $args ) {
+		$defaultInstance = array(
+			'selector'               => "WSmultipleTemplateWrapper",
+			'copy'                   => "WSmultipleTemplateMain",
+			'textarea'               => 'WSmultipleTemplateField',
+			'list'                   => 'WSmultipleTemplateList',
+			'addButtonClass'         => "WSmultipleTemplateAddAbove",
+			'addButtonClassExtra'    => "btn btn-default btn-sm",
+			'removeButtonClass'      => "WSmultipleTemplateDel",
+			'removeButtonClassExtra' => "btn btn-primary btn-sm",
+			'handleClass'            => 'ws-sortable-handle',
+			'handleClassExtra'       => 'glyphicon glyphicon-move',
+			'instanceMoveClass'      => 'ws-formgroup-sortable',
+			'draggable'              => false,
+			'instanceName'           => '',
+			'template'               => "",
+			'templateParent'         => "",
+			'copyExtra'              => ''
+		);
+
+		$defaultTranslator = array(
+			'template'            => 'template',
+			'template-parent'     => 'templateParent',
+			'name'                => 'instanceName',
+			'button-add'          => 'addButtonClass',
+			'button-remove'       => 'removeButtonClass',
+			'button-move'         => 'handleClass',
+			'button-add-extra'    => 'addButtonClassExtra',
+			'button-remove-extra' => 'removeButtonClassExtra',
+			'button-move-extra'   => 'handleClassExtra',
+			'wrapper-instance'    => 'copy',
+			'wrapper-main'        => 'selector',
+			'wrapper-main-extra'  => 'copyExtra',
+			'instance-storage'    => 'textarea',
+			'instance-list'       => 'list'
+		);
+
+		foreach ( $defaultTranslator as $from => $to ) {
+			$val = self::getArg(
+				$from,
+				$args
+			);
+			if ( $val !== false ) {
+				switch ( $from ) {
+					case "button-move":
+						$defaultInstance['draggable'] = true;
+						break;
+				}
+				$defaultInstance[$to] = $val;
+			} else {
+				switch ( $from ) {
+					case "button-add-extra":
+					case "button-move-extra":
+					case "button-remove-extra":
+					case "templateParent":
+						$defaultInstance[$to] = "none";
+						break;
+				}
+			}
+		}
+
+		return $defaultInstance;
+	}
+
+	private static function getArg( $name, $args ) {
+		if ( isset( $args[$name] ) && $args[$name] !== '' ) {
+			return $args[$name];
+		} else {
+			return false;
+		}
+	}
 
 	/**
 	 * Render WSInstance
@@ -24,74 +98,145 @@ class render {
 	 * @return string Formatted HTML
 	 */
 	public static function render_instance( $args, $innerHtml ) {
-		$ret = '';
-		$classWrapper = "WSmultipleTemplateWrapper";
-		$mainClass = "WSmultipleTemplateMain";
-		$fieldClass = 'WSmultipleTemplateField';
-		$instanceList = 'WSmultipleTemplateList';
-		$instanceName = '';
-		$template  = "";
-		$instanceMainClass = "";
-		$instanceAddButtonClass = "";
-		$instanceRemoveButtonClass = "";
-		$instanceMoveButtonClass = 'ws-formgroup-sortable';
-		$instanceMoveListClass = 'ws-sortable-handle';
-		$allowMove = true;
-
-		if( isset( $args['buttons-position'] ) ) $template = $args['template'];
-
-		if( isset( $args['template'] ) ) $template = $args['template'];
-		if( isset( $args['name'] ) ) {
-			$instanceName = $args['name'];
+		global $IP;
+		$instance       = self::instanceDefault( $args );
+		$pageWikiObject = \RequestContext::getMain()->getWikiPage();
+		$pageContent    = $pageWikiObject->getContent( RevisionRecord::RAW )->getText();
+		require_once( $IP . '/extensions/WSForm/WSForm.api.class.php' );
+		$api = new \wbApi();
+		//echo $instance['templateParent'];
+		if ( $instance['templateParent'] !== 'none' ) {
+			$templateContent = $api->getTemplate(
+				$pageContent,
+				$instance['templateParent']
+			);
+		} else {
+			$templateContent = $pageContent;
 		}
-		if( isset( $args['main-class'] ) ) $instanceMainClass = $args['main-class'];
-		if( isset( $args['add-button-class'] ) ) $instanceAddButtonClass = $args['add-button-class'];
-		if( isset( $args['remove-button-class'] ) ) $instanceRemoveButtonClass = $args['remove-button-class'];
-		if( isset( $args['disable-move'] ) ) $allowMove = false;
-
-		if( $allowMove ) {
-			$instanceMoveListClass = ' ' . $instanceMoveListClass;
-		} else $instanceMoveListClass = '';
-
-		$ret .= '<div class="' . $classWrapper . '">' . PHP_EOL;
-
-		$ret .= '<div class="hidden">' . PHP_EOL;
-
-		//$ret .= '<div data-template="' . $template . '" class="hidden ' . $fieldClass . '">';
-
-		$ret .= '<textarea rows="10" name="' . $instanceName . '"  class="hidden ' . $fieldClass . '" data-template="' . $template . '"></textarea>' . PHP_EOL;
-
-		$ret .= '<div class="' . $mainClass . ' ' . $instanceMainClass . '">' . PHP_EOL;
-
-		$ret .= $innerHtml;
-
-		$ret .= '</div></div>';
-
-		$ret .= PHP_EOL . '<div class="' . $instanceList . $instanceMoveListClass . '"></div>' . PHP_EOL . '</div>' . PHP_EOL;
+		$expl = self::pregExplode( $templateContent );
+		//echo "<pre>";
+		//var_dump( $expl );
+		//echo "</pre>";
+		$textAreaContent = '';
+		foreach ( $expl as $k => $variable ) {
+			$tmp = explode(
+				'=',
+				$variable
+			);
+			//echo "<pre>Searching for ".$instance['instanceName'];
+			//echo 'key=' . $tmp[0];
+			//echo "</pre>";
+			if ( trim( $tmp[0] ) === trim( $instance['instanceName'] ) ) {
+				//echo "ok";
+				$textAreaContent .= '{{' . $instance['template'] . self::get_string_between(
+						$expl[$k],
+						'{{' . $instance['template'],
+						'}}'
+					) . '}}';
+			}
+		}
+		//echo "<pre>";
+		//var_dump( $textAreaContent );
+		//echo "</pre>";
+		$ret = self::renderInstanceHtml(
+			$instance,
+			$innerHtml,
+			$textAreaContent
+		);
 
 		$instanceSettings = array(
-			'draggable' => $allowMove,
-			'addButtonClass' => "." . $instanceAddButtonClass,
-			'removeButtonClass' => "." . $instanceRemoveButtonClass,
-			'handleClass' => "." . trim( $instanceMoveListClass ),
-			'selector' => "." . $classWrapper,
-			'textarea' => "." . $fieldClass,
-			'list' => "." . $instanceList,
-			'copy' => "." . $mainClass
+			'draggable'         => $instance['draggable'],
+			'addButtonClass'    => "." . $instance['addButtonClass'],
+			'removeButtonClass' => "." . $instance['removeButtonClass'],
+			'handleClass'       => "." . $instance['handleClass'],
+			'selector'          => "." . $instance['selector'],
+			'textarea'          => "." . $instance['textarea'],
+			'list'              => "." . $instance['list'],
+			'copy'              => "." . $instance['copy'],
 		);
 
 		$out = \RequestContext::getMain()->getOutput();
 		$out->addJsConfigVars( array( "wsinstance" => $instanceSettings ) );
 
-
-		if( !wsform::isLoaded( 'wsinstance' ) ) {
+		if ( ! wsform::isLoaded( 'wsinstance' ) ) {
 			$js = 'wachtff( startInstance, true );';
 			wsform::includeInlineScript( $js );
 			wsform::addAsLoaded( 'wsinstance' );
 		}
 
 		return $ret;
+	}
 
+	private static function get_string_between( $string, $start, $end ) {
+		$string = " " . $string;
+		$ini    = strpos(
+			$string,
+			$start
+		);
+		if ( $ini == 0 ) {
+			return "";
+		}
+		$ini += strlen( $start );
+		$len = strrpos(
+				   $string,
+				   $end,
+				   $ini
+			   ) - $ini;
+
+		return substr(
+			$string,
+			$ini,
+			$len
+		);
+	}
+
+	private static function pregExplode( $str ) {
+		return preg_split(
+			'~\|(?![^{{}}]*\}\})~',
+			$str
+		);
+	}
+
+	private static function renderInstanceHtml( $instance, $innerHtml, $textAreaContent ) {
+		$ret = '<div class="' . $instance['selector'] . '">' . PHP_EOL;
+
+		$ret .= '<div class="hidden">' . PHP_EOL;
+
+		$ret .= '<textarea rows="10" name="' . $instance['instanceName'] . '"  class="hidden ' . $instance['textarea'] . '" data-template="' . $instance['template'] . '">' . $textAreaContent . '</textarea>' . PHP_EOL;
+
+		$ret .= '<div class="' . $instance['copy'] . ' ' . $instance['copyExtra'] . '">' . PHP_EOL;
+
+		$ret .= $innerHtml;
+
+		if ( $instance['handleClassExtra'] === 'none' ) {
+			$instance['handleClassExtra'] = '';
+		}
+
+		if ( $instance['addButtonClassExtra'] === 'none' ) {
+			$instance['addButtonClassExtra'] = '';
+		}
+
+		if ( $instance['removeButtonClassExtra'] === 'none' ) {
+			$instance['removeButtonClassExtra'] = '';
+		}
+
+		if ( $instance['handleClass'] !== 'none' && ! empty( $instance['handleClassExtra'] ) ) {
+			$ret .= '<span class="' . $instance['handleClass'] . ' ' . $instance['handleClassExtra'] . '"></span>';
+		}
+
+		if ( $instance['addButtonClass'] !== 'none' && ! empty( $instance['addButtonClassExtra'] ) ) {
+			$ret .= '<button type="button" class="' . $instance['addButtonClass'] . ' ' . $instance['addButtonClassExtra'] . '" role="button"><i class="fa fa-plus "></i></button>';
+		}
+
+		if ( $instance['removeButtonClass'] !== 'none' && ! empty( $instance['removeButtonClassExtra'] ) ) {
+			$ret .= '<button type="button" class="' . $instance['removeButtonClass'] . ' ' . $instance['removeButtonClassExtra'] . '" role="button"><i class="fa fa-times "></i></button>';
+		}
+
+		$ret .= '</div></div>';
+
+		$ret .= PHP_EOL . '<div class="' . $instance['list'] . ' ' . $instance['handleClass'] . '"></div>' . PHP_EOL . '</div>' . PHP_EOL;
+
+		return $ret;
 	}
 
 }
