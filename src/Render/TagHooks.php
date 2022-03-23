2389c2124296d3b12a136ca13e72b443bb59829f
Charlot <charlot@wikibase>
2022-03-23 10:58:19 +0100

    v1.0.0 Release Candidate 4 : id regex validation changed to allow just HTML5

--- src/Render/TagHooks.php
+++ src/Render/TagHooks.php
@@ -1593,7 +1593,7 @@ class TagHooks {
 	 */
 	private function checkValidInput( string $input ) : bool {
 		if ( ! preg_match(
-			'/^[A-Za-z]+[\w\-\:\.]*$/',
+			'/[\w\-\:\.]+$/',
 			$input
 		) ) {
 			return false;
