7617bbb07f644136646cd2d7c92dd99fb7acc0b7
designburo <info@designburo.nl>
2023-04-16 20:42:30 +0200

    v2.1.6 : Bug fix for canvas and signature uploads

--- src/Processors/Files/Canvas.php
+++ src/Processors/Files/Canvas.php
@@ -121,7 +121,7 @@ class Canvas {
 		// find any other form fields and put them into the title
 		$pageName = ContentCore::parseTitle( $pageName );
 		if ( !Config::isDebug() ) {
-			$uploadFile       = new Upload();
+			$uploadFile       = new Upload( "", [] );
 			$resultFileUpload = $uploadFile->uploadFileToWiki(
 				$uploadPath . $tmpFileName,
 				$pageName,
