f3fc2dfacb2c24c0bc435fc8d4db650fddc771ed
Charlot <charlot@wikibase>
2024-11-15 10:35:27 +0100

    # Conflicts: #	README.md #	extension.json

--- Modules/FlexForm.general.js
+++ Modules/FlexForm.general.js
@@ -627,7 +627,11 @@ function wsform (btn, callback = 0, preCallback = 0, showId = 0) {
 			if (result.status === 'ok') {
 				statusType = 'success';
 				if ( mwonsuccess === 'Saved successfully' ) {
-					statusMsg = result.message;
+					if ( result.message !== "" ) {
+						statusMsg = result.message;
+					} else {
+						statusMsg = mwonsuccess;
+					}
 				} else {
 					statusMsg = mwonsuccess;
 				}
@@ -749,7 +753,10 @@ const ffCalc = (element = null) => {
 	}
 }
 
-let ffDecryptObj = {};
+if ( typeof window.ffDecryptObj === 'undefined' ) {
+	var ffDecryptObj = {};
+}
+
 const fetchAllDecrypt = async () => {
 	if ( wgFlexFormSecure === false ) {
 		return;
@@ -780,14 +787,14 @@ const fetchAllDecrypt = async () => {
 	const data = await result;
 	const res = data.flexform.result.data;
 	for (let i = 0; i < res.length; i++ ) {
-		ffDecryptObj[jsonObj[i]] = res[i];
+		window.ffDecryptObj[jsonObj[i]] = res[i];
 	}
 }
 
 const getDecrypt = ( txt ) => {
 	if ( wgFlexFormSecure === false ) return txt;
-	if ( !ffDecryptObj[txt] ) return txt;
-	return ffDecryptObj[txt];
+	if ( !window.ffDecryptObj[txt] ) return txt;
+	return window.ffDecryptObj[txt];
 }
 
 /**
