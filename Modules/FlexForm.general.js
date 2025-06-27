36fd217f733e6e727239bfeb0a928a62f5811e24
Designburo <ccobben@gmail.com>
2025-06-27 15:32:03 +0200

    return of wachtff

--- Modules/FlexForm.general.js
+++ Modules/FlexForm.general.js
@@ -53,6 +53,40 @@ function ffFindFormElementValueByName( form, name ) {
 	} else return "";
 }
 
+/**
+ * Holds further JavaScript execution intull jQuery is loaded
+ * @param method string Name of the method to call once jQuery is ready
+ * @param both bool if true it will also wait until MW is loaded.
+ */
+function wachtff (method, both = false ) {
+	//console.log('wacht ff op jQuery..: ' + method.name );
+	if (window.jQuery) {
+		if (both === false) {
+			//console.log( 'ok JQuery active.. lets go!' );
+			method()
+		} else {
+			if (window.mw) {
+				var scriptPath = mw.config.get('wgScript')
+				if (scriptPath !== null && scriptPath !== false) {
+					method()
+				} else {
+					setTimeout(function () {
+						wachtff(method, true)
+					}, 250)
+				}
+			} else {
+				setTimeout(function () {
+					wachtff(method, true)
+				}, 250)
+			}
+		}
+	} else {
+		setTimeout(function () {
+			wachtff(method)
+		}, 50)
+	}
+}
+
 function wsformShowOnSelect () {
 	var lst = mw.config.get('wsformConfigVars')
 	if (lst === null) return
