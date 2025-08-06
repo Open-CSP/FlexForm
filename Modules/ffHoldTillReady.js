d6fca3566100f30b582fd00a44aee7a37f475d40
C. Cobben <info@designburo.nl>
2025-08-06 10:22:59 +0200

    Rel1 35 2 4 3 (#83)
    * when not allowed to edit a form, also check for tinymceedit and veditor
    
    * v2.4.3 : Double definition of JavaScript constants removed. console message removed.
    
    * straightened code
    
    * v2.4.3 Double definition of JavaScript constants removed. console message removed.
    
    ---------
    
    Co-authored-by: Charlot <charlot@wikibase>

--- Modules/ffHoldTillReady.js
+++ Modules/ffHoldTillReady.js
@@ -1,16 +1,23 @@
+
+function isFFInitiated() {
+	return typeof window.FlexFormInitiated !== 'undefined';
+}
+
+function FFInitiated() {
+	if ( typeof window.FlexFormInitiated === 'undefined' ) {
+		window.FlexFormInitiated = true;
+	}
+}
+
 /**
- * Holds further JavaScript execution intull jQuery is loaded
+ * Holds further JavaScript execution until jQuery is loaded
  * @param method string Name of the method to call once jQuery is ready
  * @param both bool if true it will also wait until MW is loaded.
  */
-
-var FFAlreadyRun = false;
-
-
-function ffHoldTillReady( method, both= true ) {
+function ffHoldTillReady( method, both = true ) {
 	if ( window.jQuery ) {
 		if ( both === false ) {
-			if ( window.wsform ) {
+			if ( isFFInitiated() ) {
 				method();
 			} else {
 				setTimeout( function () {
@@ -19,23 +26,8 @@ function ffHoldTillReady( method, both= true ) {
 			}
 		} else {
 			if ( window.mw ) {
-				var scriptPath = mw.config.get( 'wgScript' )
-				if ( scriptPath !== null && scriptPath !== false ) {
-					scriptPath = scriptPath.replace ('/index.php', '' )
-					if ( window.wsform ) {
-						method();
-					} else {
-						if ( window.FFAlreadyRun === false ) {
-							window.FFAlreadyRun = true;
-							$.getScript(scriptPath + '/extensions/FlexForm/Modules/FlexForm.general.js').done(function () {
-								method()
-							});
-						} else {
-							setTimeout( function () {
-								ffHoldTillReady( method, true )
-							}, 250 )
-						}
-					}
+				if ( isFFInitiated() ) {
+					method();
 				} else {
 					setTimeout( function () {
 						ffHoldTillReady( method, true )
@@ -55,7 +47,6 @@ function ffHoldTillReady( method, both= true ) {
 }
 
 function ffDummyInit(){
-	window.FFAlreadyRun = true;
 }
 
 ffHoldTillReady( ffDummyInit );
